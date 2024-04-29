b630557aaf608b2fb620d4b7735cd90066fef26e
saulvargas88 <saulvargascosta@hotmail.com>
2024-04-29 20:48:36 -0300

    estrutura base

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './componentes/Home';
+import './global.css'
 
 function App() {
   return (
--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import '../componentes/Home.css';
+import fotoPerfil from '../assents/FotoPerfil.png';
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
 
-export default Home
+  return (
+    <div id="container">
+      <div className="containerOne">
+        <div className="divImg">
+          <img
+            src={fotoPerfil}
+            alt="fotoPerfil"
+            width="150"
+            className="imgPerfil"
+          />
+          <div className="titleTop">
+            <p className="titleTopText">Saul Vargas</p>
+            <p className="titleTopTextDev">Desenvolvedor</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
